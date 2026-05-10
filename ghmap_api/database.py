816d60a01957942cd76af976d61e1658e08c2d26
jwfraustro <36318163+jwfraustro@users.noreply.github.com>
2026-05-10 10:50:02 -0400

    update project for railway deployment

--- ghmap_api/database.py
+++ ghmap_api/database.py
@@ -1,9 +1,11 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
-DATABASE_URL = "sqlite:///./ghmap.db"
+DATABASE_URL = os.environ.get("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
 
 
